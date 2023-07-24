d75c7035998202290f4c102244ca0357c02f3e3d
Eswar12323 <eswarn955@gmail.com>
2023-07-24 07:03:44 +0000

    enter

---
+++ reactapp/src/src/products.js
@@ -0,0 +1,99 @@
+import product1 from "./assets/products/cheese.jpg";
+import product2 from "./assets/products/bread.jpg";
+import product3 from "./assets/products/coffeee.jpg";
+import product4 from "./assets/products/daal1.jpeg";
+import product5 from "./assets/products/oil.jpeg";
+import product6 from "./assets/products/egg.jpg";
+import product7 from "./assets/products/nuts.jpg";
+import product8 from "./assets/products/salt.jpg";
+import product9 from "./assets/products/daal2.jpg";
+import product10 from "./assets/products/daal3.jpg";
+import product11 from "./assets/products/boost.jpg";
+import product12 from "./assets/products/daal4.webp";
+import product13 from "./assets/products/horlicks.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Cheese",
+    price: 80.02,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Bread",
+    price: 50.03,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Coffee",
+    price: 70.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Chinna Daal",
+    price: 100.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Gold Winner",
+    price: 130.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Egg",
+    price: 30.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Cashew Nuts",
+    price: 200.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Salt",
+    price: 20.0,
+    productImage: product8,
+  },
+
+  {
+    id: 9,
+    productName: "Arhar Daal",
+    price: 120.0,
+    productImage: product9,
+  },
+
+  {
+    id: 10,
+    productName: "Masoor Daal",
+    price:180.0,
+    productImage: product10,
+  },
+
+  {
+    id: 11,
+    productName: "Boost",
+    price: 250.0,
+    productImage: product11,
+  },
+
+  {
+    id: 12,
+    productName: "Chana Daal",
+    price: 150.0,
+    productImage: product12,
+  },
+
+  {
+    id: 13,
+    productName: "Horlicks Chocolate",
+    price: 180.0,
+    productImage: product13,
+  },
+];
